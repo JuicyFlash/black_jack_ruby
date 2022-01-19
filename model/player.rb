6feeb866c8946f26df3cc1a45e8dafd016272880
JuicyFlash <tankedet@bk.ru>
2022-01-19 16:30:40 +0300

    Подсчёт очков игрока

--- model/player.rb
+++ model/player.rb
@@ -2,7 +2,8 @@ require_relative 'deck'
 
 class Player
 
-  attr_accessor :name, :bank, :deck
+  attr_accessor :name, :bank, :deck, :controlled_by
+  attr_reader :score
 
   def initialize(name, bank, deck)
     self.name = name
@@ -10,8 +11,26 @@ class Player
     self.deck = deck
   end
 
-  def take_cards(*cards)
-    deck.put_cards(cards)
+  def need_card?
+    deck.cards.length < 3
+  end
+
+  def score
+    result = 0
+    a_value_count = 0
+    deck.cards.each do |card|
+      result += card.value
+      a_value_count += 1 if (card.alternative_value - card.value).positive?
+    end
+    while a_value_count.positive?
+      result += 9 * a_value_count if result + 9 * a_value_count <= 21
+      a_value_count -= 1
+    end
+    result
+  end
+
+  def take_card(card)
+    deck.put_cards(card) if need_card?
   end
 
   def drop_cards
